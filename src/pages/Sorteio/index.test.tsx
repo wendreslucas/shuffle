5b1d34ad67279dda71bc7938aec2792cd1272a0f
Wendres Lucas Araujo <91164904+wendreslucas@users.noreply.github.com>
2022-06-29 16:47:48 -0300

    corrigindo teste

--- src/pages/Sorteio/index.test.tsx
+++ src/pages/Sorteio/index.test.tsx
@@ -36,7 +36,7 @@ describe('Na pagina de sorteio', () => {
     )
 
     const opcoes = screen.queryAllByRole('option')
-    expect(opcoes).toHaveLength(participantes.length )
+    expect(opcoes).toHaveLength(participantes.length + 1)
   })
 
   test('o amigo secreto é exibido quando solicitado', () => {
